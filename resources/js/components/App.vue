bb3852baec8d3fc794ff2ec4b0bb7a6080437dfb
Carlos Cruz San Román <carlos.cruz@sngular.com>
2020-01-22 09:29:37 +0100

    Create base component with tailwind, slots and conditional classes

--- resources/js/components/App.vue
+++ resources/js/components/App.vue
@@ -1,14 +1,35 @@
 <template>
-    <div class="h-screen flex items-center justify-center">
-        <div class="w-32 h-32 bg-blue-500 rounded-full">
-        </div>
+    <div>
+        <section class="m-16">
+            <h1 class="text-5xl font-semibold mb-8">Panels</h1>
+            <base-panel class="mb-8">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto aspernatur commodi deserunt, eius
+                eos eveniet laudantium libero molestiae mollitia, pariatur perspiciatis quibusdam saepe totam vel vero.
+                Beatae doloribus iusto totam.
+            </base-panel>
+            <base-panel class="mb-8 flex" no-padding>
+                <div class="bg-blue-500 w-64"></div>
+                <div class="px-6 py-4 flex-1">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias aliquam aperiam beatae earum et explicabo fugiat magnam mollitia qui sint temporibus ullam, vel voluptates voluptatibus voluptatum? Ab autem culpa optio?
+                </div>
+            </base-panel>
+            <base-panel class="mb-8" title="My Title" subtitle="Some metadata">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto aspernatur commodi deserunt, eius
+                eos eveniet laudantium libero molestiae mollitia, pariatur perspiciatis quibusdam saepe totam vel vero.
+                Beatae doloribus iusto totam.
+            </base-panel>
+        </section>
     </div>
 </template>
 
 <script>
-export default {
+    import BasePanel from "./BasePanel";
 
-}
+    export default {
+        components: {
+            BasePanel
+        }
+    }
 </script>
 
 <style>
---
+++ resources/js/components/BasePanel.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="bg-white rounded-lg shadow-md overflow-hidden" :class="{ 'px-6 py-4' : ! noPadding }">
+        <div class="mb-6" v-if="title">
+            <h1 class="text-gray-900 text-xl" v-text="title"></h1>
+            <div class="text-gray-600 text-sm uppercase tracking-wide font-semibold mt-1" v-text="subtitle"></div>
+        </div>
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        title: String,
+        subtitle: String,
+        noPadding: Boolean,
+    }
+}
+</script>
+
+<style>
+
+</style>
